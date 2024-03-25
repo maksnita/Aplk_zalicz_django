bb97f48cfd9c383453f504f2730b3259a6a5d542
maksnita <maksymilian.nita@op.pl>
2024-03-25 22:20:01 +0100

    Wersja aplikacji przed testami

--- apk_zalicz/notatnik/views.py
+++ apk_zalicz/notatnik/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 from .forms import *
-from django.utils import timezone
 
 def notatnik(request):
     form = NotatkaForm()
@@ -13,6 +12,12 @@ def notatnik(request):
             Przedmiot.objects.create(nazwa=nazwa_nowego_przedmiotu)
             przedmioty = Przedmiot.objects.all()
             return render(request, 'notatnik/notatnik.html', {'przedmioty': przedmioty})
+        elif action == 'usun_przedmiot':
+            przedmiot_id = request.POST.get('przedmiot_usun')
+            przedmiot = get_object_or_404(Przedmiot, id=przedmiot_id)
+            przedmiot.delete()
+            przedmioty = Przedmiot.objects.all()
+            return render(request, 'notatnik/notatnik.html', {'przedmioty': przedmioty})
         elif action == 'zapisz_notatke':
             form = NotatkaForm(request.POST)
             if form.is_valid():
@@ -22,7 +27,9 @@ def notatnik(request):
     przedmioty = Przedmiot.objects.all()
     return render(request, 'notatnik/notatnik.html', {'form': form, 'przedmioty': przedmioty})
 
+
 def baza(request):
+
     if request.method == 'POST':
         action = request.POST.get('action')
         if action == 'usun_notatke':
@@ -30,7 +37,14 @@ def baza(request):
             notatka = get_object_or_404(Notatka, id=note_id)
             notatka.delete()
             return redirect('baza')
+        elif action == 'edytuj_notatke':
+            note_id = request.POST.get('note_id')
+            edited_content = request.POST.get('edited_content')
+            notatka = get_object_or_404(Notatka, id=note_id)
+            notatka.tresc = edited_content
+            notatka.save()
+            return redirect('baza')
 
     przedmioty = Przedmiot.objects.all()
-    notatki = Notatka.objects.all()
+    notatki = Notatka.objects.order_by('-data_utworzenia')
     return render(request, 'notatnik/baza.html', {'przedmioty': przedmioty, 'notatki': notatki})
